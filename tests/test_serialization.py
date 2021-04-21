832f1e903ea2683bd17b7bfca25c4543db3a10ed
Jan Betley <jan.betley@golem.network>
2021-04-21 13:43:36 +0200

    tests refactor - no more global out_req

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -8,59 +8,51 @@ from tempfile import NamedTemporaryFile
 import catchall_server
 from serializable_request import Request
 
-out_req = None
+Session.register(BASE_URL, catchall_server.app)
 
 
-@pytest.fixture
-def session():
-    Session.register(BASE_URL, catchall_server.app)
-    return Session()
+def extract_request():
+    return Request.from_flask_request().as_requests_request()
 
 
-def pseudo_forward():
+def extract_and_serialize():
     '''
-    Send request to flask.
-    Recreate request using flask internal request context.
-
-    This tests if we use flask.request correctly.
-    '''
-    global out_req
-    out_req = Request.from_flask_request().as_requests_request()
-    return {}, 200
-
-
-def pseudo_forward_with_file():
-    '''
-    Send request.
-    Recreate request from flask internal request context.
-    Save request to file.
-    Read request from this file.
-
-    Assuming pseudo_forward finds no errors, this tests if we correctly
+    Assuming extract_request finds no errors, this tests if we correctly
     serialize/deserialize the request.
     '''
-    global out_req
     req = Request.from_flask_request()
 
     with NamedTemporaryFile() as f:
         req.to_file(f.name)
-        out_req = Request.from_file(f.name).as_requests_request()
-    return {}, 200
+        return Request.from_file(f.name).as_requests_request()
 
 
-@pytest.mark.parametrize('forward_func', [pseudo_forward, pseudo_forward_with_file])
+def process_request(extract_func, prepped):
+    out_req = None
+
+    def pseudo_forward_func():
+        nonlocal out_req
+        out_req = extract_func()
+        return {}, 200
+
+    catchall_server.forward_request = pseudo_forward_func
+
+    res = Session().send(prepped)
+    return res, out_req
+
+
+@pytest.mark.parametrize('extract_func', [extract_request, extract_and_serialize])
 @pytest.mark.parametrize('src_req', sample_requests)
-def test_serialization(forward_func, session, src_req):
-    catchall_server.forward_request = forward_func
+def test_serialization(extract_func, src_req):
     prepped = src_req.prepare()
 
-    res = session.send(prepped)
+    res, out_req = process_request(extract_func, prepped)
+    out_prepped = out_req.prepare()
 
     #   Fail --> traceback in serialization
     assert res.status_code == 200
 
     #   Fail --> incorrect serialization
-    out_prepped = out_req.prepare()
     assert prepped.method == out_prepped.method
     assert prepped.url == out_prepped.url
     assert clean_headers(prepped.headers) == clean_headers(out_prepped.headers)

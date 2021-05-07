4778663470ed94eee2123b516fa49c7c2ca09d0e
Jan Betley <jan.betley@golem.network>
2021-05-07 13:46:55 +0200

    new YagnaConnector prototype

--- catchall_server.py
+++ catchall_server.py
@@ -1,10 +1,19 @@
 from quart import Quart
+from yagna_connector import YagnaConnector
 
+#   TODO: do we want to use hypercorn?
+import hypercorn
+hypercorn.Config.startup_timeout = 999
 
 HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 
 app = Quart(__name__)
 
+@app.before_serving
+async def start_provider():
+    yagna = YagnaConnector()
+    await yagna.init_provider()
+    app.yagna = yagna
 
 async def forward_request():
     raise NotImplementedError
@@ -14,3 +23,6 @@ async def forward_request():
 async def catch_all(path):
     res = await forward_request()
     return res
+
+if __name__ == '__main__':
+    app.run()
